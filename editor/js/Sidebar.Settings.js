5b07269528146ea71b8e8514e8085adb4c8960b5
Guy Sandler <guy@sandler.co.il>
2025-04-19 19:50:40 -0700

    Removed Translations

--- editor/js/Sidebar.Settings.js
+++ editor/js/Sidebar.Settings.js
@@ -15,41 +15,6 @@ function SidebarSettings( editor ) {
 	settings.setPaddingTop( '20px' );
 	container.add( settings );
 
-	// language
-
-	const options = {
-		en: 'English',
-		fr: 'Français',
-		zh: '中文',
-		ja: '日本語',
-		ko: '한국어',
-	};
-
-	const languageRow = new UIRow();
-	const language = new UISelect().setWidth( '150px' );
-	language.setOptions( options );
-
-	if ( config.getKey( 'language' ) !== undefined ) {
-
-		language.setValue( config.getKey( 'language' ) );
-
-	}
-
-	language.onChange( function () {
-
-		const value = this.getValue();
-
-		editor.config.setKey( 'language', value );
-
-	} );
-
-	languageRow.add( new UIText( strings.getKey( 'sidebar/settings/language' ) ).setClass( 'Label' ) );
-	languageRow.add( language );
-
-	settings.add( languageRow );
-
-	//
-
 	container.add( new SidebarSettingsShortcuts( editor ) );
 	container.add( new SidebarSettingsHistory( editor ) );
 
