a235db760d1520f830a125efa48b337d2c122f3c
awehero <147273991+awehero@users.noreply.github.com>
2025-04-19 22:20:52 -0400

    Update Sidebar.Project.App.js

--- editor/js/Sidebar.Project.App.js
+++ editor/js/Sidebar.Project.App.js
@@ -496,16 +496,18 @@ function SidebarProjectApp( editor ) {
 				case 'B':
 					geometry = new THREE.BoxGeometry( 1, 1, 1, 1, 1, 1 );
 					loader = new THREE.TextureLoader();
-					loader.load( 'images/textures/bright.png', function ( texture ) {
-
-						texture.colorSpace = THREE.SRGBColorSpace;
-						material = new THREE.MeshBasicMaterial( { map: texture } );
-						mesh = new THREE.Mesh( geometry, material );
-						mesh.name = 'Box';
-						mesh.userData.CustomTexture = [ 'images/textures/bright.png' ];
-
-					} );
-					break;
+					return new Promise((resolve, reject) => {
+						loader.load( 'images/textures/bright.png', function ( texture ) {
+					        	texture.colorSpace = THREE.SRGBColorSpace;
+					      		material = new THREE.MeshBasicMaterial( { map: texture } );
+					      		mesh = new THREE.Mesh( geometry, material );
+					      		mesh.name = 'Box';
+					      		mesh.userData.CustomTexture = [ 'images/textures/bright.png' ];
+					      		resolve(mesh);
+					    	}, undefined, reject);
+				  	});
+				break;
+					
 				case 'C':
 					geometry = new THREE.ConeGeometry( .5, 1, 16 );
 					material = new THREE.MeshBasicMaterial( { color: 0xD52B2B } );
