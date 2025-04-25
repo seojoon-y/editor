3108bfa2e332b573e40dfd7b8b05de72d58bf6ac
awehero <awebricks79@gmail.com>
2025-04-25 10:59:47 -0400

    Added link to google doc

--- editor/js/Menubar.Status.js
+++ editor/js/Menubar.Status.js
@@ -39,9 +39,10 @@ function MenubarStatus( editor ) {
 
 	} );
 
-	const version = new UIText( 'DodoEditor Beta 0.1.0' );
+	const version = new UIAnchor( 'How to get started' );
 	version.setClass( 'title' );
 	version.setOpacity( 0.65 );
+	version.setHref( 'https://docs.google.com/document/d/1qv7PBwLh8m6FqIqtHme363KDS9KRoEMxF3haKg3ciR4' );
 	container.add( version );
 
 	return container;
