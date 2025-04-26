c7e81a86bc671f3c065651af58e134ce070dbfea
onionfist <seojoony9000@gmail.com>
2025-04-26 12:30:44 +0900

    remove UIAnchor

--- editor/js/Menubar.Status.js
+++ editor/js/Menubar.Status.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 
-import { UIPanel, UIAnchor } from './libs/ui.js';
+import { UIPanel } from './libs/ui.js';
 import { UIBoolean } from './libs/ui.three.js';
 
 function MenubarStatus( editor ) {
@@ -39,11 +39,13 @@ function MenubarStatus( editor ) {
 
 	} );
 
+	/* commented out by Jason on Apr because UIAnchor does not exist
 	const version = new UIAnchor( 'How to get started' );
 	version.setClass( 'title' );
 	version.setOpacity( 0.65 );
 	version.setHref( 'https://docs.google.com/document/d/1qv7PBwLh8m6FqIqtHme363KDS9KRoEMxF3haKg3ciR4' );
 	container.add( version );
+	*/
 
 	return container;
 
